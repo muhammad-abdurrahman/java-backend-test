ac48730b60783957700e3407a64d5db05573129c
Muhammad Abdurrahman <muhammad.abdurrahman.uk@gmail.com>
2018-04-16 19:00:30 +0100

    initialize repo

---
+++ src/main/java/com/yoti/springcleaning/SpringCleaningApplication.java
@@ -0,0 +1,12 @@
+package com.yoti.springcleaning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCleaningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCleaningApplication.class, args);
+    }
+}
---
+++ src/test/java/com/yoti/springcleaning/SpringCleaningApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yoti.springcleaning;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCleaningApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
