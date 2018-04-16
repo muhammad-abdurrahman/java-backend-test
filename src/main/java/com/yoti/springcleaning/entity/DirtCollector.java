d8196c3e6fadac60fefb51f5a7ae2ae274d0a750
Muhammad Abdurrahman <muhammad.abdurrahman.uk@gmail.com>
2018-04-16 21:27:11 +0100

    add normalization of current location within room boundary

--- src/main/java/com/yoti/springcleaning/entity/DirtCollector.java
+++ src/main/java/com/yoti/springcleaning/entity/DirtCollector.java
@@ -1,5 +1,6 @@
 package com.yoti.springcleaning.entity;
 
+import com.yoti.springcleaning.enums.CardinalDirections;
 import lombok.Data;
 
 @Data
@@ -11,5 +12,33 @@ public class DirtCollector {
     public DirtCollector(Room room, Coordinate initialCoordinate) {
         this.room = room;
         this.currentLocationInRoom = initialCoordinate;
+    }
+
+    public Coordinate moveInDirection(CardinalDirections direction){
+        this.currentLocationInRoom.translateByDisplacementCoordinate(direction.getDisplacementCoordinates());
+        return normalizeCurrentLocationWithinBoundary();
+    }
+
+    private Coordinate normalizeCurrentLocationWithinBoundary(){
+        if (this.currentLocationInRoom.getX() > 0
+                && this.currentLocationInRoom.getX() <= this.room.getMaxCoords().getX()
+                && this.currentLocationInRoom.getY() > 0
+                && this.currentLocationInRoom.getY() <= this.room.getMaxCoords().getY()){
+            return this.currentLocationInRoom;
+        }
+
+        if (this.currentLocationInRoom.getX() < 0) {
+            this.currentLocationInRoom.setX(0);
+        } else { // this.currentLocationInRoom.getX() > this.room.getMaxCoords().getX()
+            this.currentLocationInRoom.setX(this.room.getMaxCoords().getX());
+        }
+
+        if (this.currentLocationInRoom.getY() < 0){
+            this.currentLocationInRoom.setY(0);
+        } else { // this.currentLocationInRoom.getY() > this.room.getMaxCoords().getY()
+            this.currentLocationInRoom.setY(this.room.getMaxCoords().getY());
+        }
+
+        return this.currentLocationInRoom;
     }
 }
