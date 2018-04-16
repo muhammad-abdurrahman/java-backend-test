c0cef9570afddd155ff368effbf6f8d621699ee1
Muhammad Abdurrahman <muhammad.abdurrahman.uk@gmail.com>
2018-04-16 19:18:32 +0100

    add basic folder structure

--- src/main/java/com/yoti/springcleaning/_config/SpringCleaningApplication.java
+++ src/main/java/com/yoti/springcleaning/_config/SpringCleaningApplication.java
@@ -1,9 +1,11 @@
-package com.yoti.springcleaning;
+package com.yoti.springcleaning._config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.yoti.springcleaning")
 public class SpringCleaningApplication {
 
     public static void main(String[] args) {
